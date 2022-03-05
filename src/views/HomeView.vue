5d1c7594129c9af59fe504e19d0b291114c36d4d
metlykoff <ochenmoguchiy@yandex.ru>
2022-03-05 22:00:33 +0300

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class = "header">
+      <h1>Интеграция внешних платформ в цифровой МИЭМ</h1>
+      <h3>Единый онлайн-сервис для дистанционного обучения</h3>
+      <button>Перейти</button>
+    
+    <div>
+    видео
+    </div>
+    <div>
+      <h1>5 видеоплатформ для проведения занятий</h1>
+      <!-- <div><img src=".//images.labels.png" alt=""></div> -->
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style>
+.header{
+  text-align: center;
+}
+button{
+  color: black;
+  text-decoration: none;
+}
+
+</style>
